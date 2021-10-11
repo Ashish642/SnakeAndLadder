9e0f468cc6066b12b2b56515007f98c77ac4fc7c
Ashish <ashishkumarvish.432@gmail.com>
2021-10-11 13:03:23 +0530

    Welcome To

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,4 +1,9 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int position = 0; // initialization of position
+        System.out.println("Welcome to Snake and Ladder Problem");
+    }
+
 }
