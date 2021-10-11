e9e0ca26131f2bf8ee324ff2dfae408e3daab572
Ashish <ashishkumarvish.432@gmail.com>
2021-10-11 14:45:59 +0530

    wining position

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,46 +2,59 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 
-     static int position = 0; // initialization of position
+    final static int Win_POSITION = 100;
+    // variable
+    static int position = 0; // intialize position
+    static int getPosition = 0;
 
     public static void main(String[] args) {
         SnakeAndLadder snakeLad = new SnakeAndLadder();
-        snakeLad.playerOption(); // call method for player option
+        snakeLad.playerOption();
 
     }
-     public static int rolldevice() {
+
+    public static int rolldevice() {
         /* Method used Math class
            To genrating Random number b/w 1 to 6
          */
-         int device = (int) (Math.floor(Math.random() * 10) % 6 +1);
-         System.out.println("Device Roll is : " +device);
-         return device;
-     }
-      public static void playerOption() {
+        int device = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Device Roll is : " + device);
+        return device;
+    }
+
+    public static void playerOption() {
         /* Method use :
-          use Random Math class to genrating number b/w 0 to 2
+          use Random Math class to generating number b/w 0 to 2
           use switch to check player option
+          adding while loop to check win position
        */
-          int device = rolldevice();
-          int option = (int) (Math.floor(Math.random() * 10) %3);
-          System.out.println("Player roll option is : " + option);
-          System.out.println("0.number Player 1. Ladder 2. Snake");
-          switch (option) {
-              case 0:
-                   System.out.println("No Player");
-              case 1:
-                  System.out.println("Ladder");
-                  position+= device;
-                  break;
-              case 2:
-                  System.out.println("Snake");
-                  position-= device;
-                  break;
-              default:
-                  System.out.println("Sytem error");
+        while (position < Win_POSITION) {
+            int device = rolldevice();
+            int option = (int) (Math.floor(Math.random() * 10) % 3);
+            System.out.println("Player roll option is : " + option);
+            System.out.println("0.number Player 1. Ladder 2. Snake");
+            switch (option) {
+                case 0:
+                    System.out.println("no player is on" + position);
+                    break;
+                case 1:
+                    position += device;
+                    if ((position - device) < getPosition) {
+                        position = getPosition;
 
-          }
+                    }
+                    System.out.println("Ladder is on" + position + "position");
+                    break;
+                case 2:
+                    position -= device;
+                    System.out.println("Snake is on " + position + "position");
+                    break;
+                default:
+                    System.out.println("Sytem error");
 
-      }
+            }
 
+        }
+
+    }
 }
