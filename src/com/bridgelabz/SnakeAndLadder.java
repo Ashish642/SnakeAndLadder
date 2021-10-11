1cd82ba53dba7c5ff85685fb8c705de0afdc23f2
Ashish <ashishkumarvish.432@gmail.com>
2021-10-11 13:31:49 +0530

    Roll the Device

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,9 +1,22 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+
+    int position = 0; // initialization of position
+
     public static void main(String[] args) {
-        int position = 0; // initialization of position
-        System.out.println("Welcome to Snake and Ladder Problem");
+        SnakeAndLadder snakeLad = new SnakeAndLadder();
+        int deviceValu = snakeLad.rolldevice(); // call method to store method return valu
+
+        System.out.println(+deviceValu);
     }
+     public int rolldevice() {
+        /* Method used Math class
+           To genrating Random number b/w 1 to 6
+         */
+         int device = (int) (Math.floor(Math.random() * 10) % 6 +1);
+         System.out.println("Device Roll is : " +device);
+         return device;
+     }
 
 }
